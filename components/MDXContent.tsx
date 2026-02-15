46230f04299290cbe2da6f8c040fa73837715212
Siddharth Shringi <siddharthshringi@gmail.com>
2026-02-15 19:23:31 +0530

    Complete first draft

--- components/MDXContent.tsx
+++ components/MDXContent.tsx
@@ -1,6 +1,7 @@
 import { JSX } from 'react';
 import { MDXRemote, type MDXRemoteProps } from 'next-mdx-remote-client/rsc';
 import rehypePrettyCode from 'rehype-pretty-code';
+import remarkGfm from 'remark-gfm';
 
 const rehypePrettyCodeOptions = {
   theme: {
@@ -17,6 +18,7 @@ export default function MDXContent(props: JSX.IntrinsicAttributes & MDXRemotePro
       {...props}
       options={{
         mdxOptions: {
+          remarkPlugins: [remarkGfm],
           rehypePlugins: [[rehypePrettyCode, rehypePrettyCodeOptions]],
         },
         ...props.options,
